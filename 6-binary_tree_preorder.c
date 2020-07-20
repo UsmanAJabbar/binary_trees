45deba12e7035b7cd4e2f2c220fe3cdfbe430280
UsmanGTA <usmangta@gmail.com>
2020-07-20 19:52:10 +0000

    added initial commit for task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/* binary_tree_preorder - prints out the data in the tree
+ * using pre-order traversal
+ * @tree: pointer to the root of the tree
+ * @func: function responsible to processing input
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
