579ffa452141e74a0dac2a1c179af819a8fbea2b
UsmanGTA <usmangta@gmail.com>
2020-07-20 17:32:43 +0000

    added initial files for master repo

---
+++ binary_trees.h
@@ -0,0 +1,48 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/* -----------------------------------------*/
+/*	  		BINARY TREE STRUCT			    */
+/* -----------------------------------------*/
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+/* ------ BINARY SEARCH TREE --------*/
+typedef struct binary_tree_s binary_tree_t;
+
+/* --------- AVL TREE ------------*/
+typedef struct binary_tree_s bst_t;
+
+/* -------- MAX BINARY HEAP -------- */
+typedef struct binary_tree_s heap_t;
+
+
+/* -----------------------------------------*/
+/*	  		PRINT BINARY TREE			    */
+/* -----------------------------------------*/
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+
+/* -----------------------------------------*/
+/*	  		MANDATORY TASK DEF			    */
+/* -----------------------------------------*/
+void binary_tree_print(const binary_tree_t *);
+
+#endif
