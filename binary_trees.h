1af970ae70f6687dcc18063ca3a14aa36f9afd12
jicruz96 <accounts@jicruz.com>
2020-07-21 14:19:25 +0000

    Initial commit of file 15, its main; add prototype(s) to header

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#define max(a, b) (((a) > (b) ? (a) : (b)))
+#define MAX(a, b) (((a) > (b) ? (a) : (b)))
+#define ISLEAF(a) ((a) && !(a)->left && !(a)->right)
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -53,5 +54,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
