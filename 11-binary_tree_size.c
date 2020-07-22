e9eeabb9f759d6f8078d5793d2015f9529aa45dd
jicruz96 <accounts@jicruz.com>
2020-07-22 19:42:17 +0000

    Add documentation to file 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
-/*
+/**
+ * binary_tree_size - returns size of a binary tree
+ * @tree: pointer to tree
+ * Return: size of tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
