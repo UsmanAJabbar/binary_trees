6301d978ee502ebc15020afd32f10d7fd26405f6
jicruz96 <accounts@jicruz.com>
2020-07-22 19:54:03 +0000

    change approach when balance == 0 on file 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,26 +2,24 @@
 #include "9-binary_tree_height.c"
 
 /**
- * binary_tree_balance - returns the balance
- * betweeen trees.
+ * binary_tree_balance - checks if a tree is balanced
+ * @tree: pointer to tree
+ * Return: 1 if balanced | o if unbalanced or NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int r, l;
+	int r, l, balance;
 
 	if (!tree)
 		return (0);
 
 	r = binary_tree_height(tree->right);
 	l = binary_tree_height(tree->left);
+	balance = l - r;
 
 	/* Check false zeros */
-	if (l - r == 0)
-	{
-		if (tree->left && !tree->right)
-			return (1);
-		if (!tree->left && tree->right)
-			return (-1);
-	}
-	return (l - r);
+	if (balance == 0)
+		balance = ISLEAF(tree->left) - ISLEAF(tree->right);
+
+	return (balance);
 }
