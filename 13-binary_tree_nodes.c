ccc5f4bc1256ca3c9e34f68b9ec475a387222499
jicruz96 <accounts@jicruz.com>
2020-07-22 19:53:39 +0000

    Make file 13 comply with style requirements

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,21 +1,19 @@
 #include "binary_trees.h"
-#include "4-binary_tree_is_leaf.c"
 
-/*
- * binary_tree_nodes - counts all the nodes
- * that have at least one child in it
+/**
+ * binary_tree_nodes - counts all non-leaf nodes
  * @tree: binary tree
- * Return: Returns the number of nodes
- */
+ * Return: number of non-leaf nodes
+ **/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t left_count = 0, right_count = 0;
 
-	 if (!tree)
+	if (!tree)
 		return (0);
 
 	left_count = binary_tree_nodes(tree->left);
 	right_count = binary_tree_nodes(tree->right);
 
-	return (left_count + right_count + !binary_tree_is_leaf(tree));
+	return (left_count + right_count + !ISLEAF(tree));
 }
