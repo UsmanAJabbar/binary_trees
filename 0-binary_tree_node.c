ba144ed3c736b94d67e0423065e53cb03278e8bd
UsmanGTA <hi@usmanjabbar.com>
2021-03-04 05:31:16 +0500

    Added styling fixes for consistency

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,13 +11,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	if(!node)
-		return(NULL);
+	if (!node)
+		return (NULL);
 
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
 	node->parent = parent;
 
-	return(node);
+	return (node);
 }
